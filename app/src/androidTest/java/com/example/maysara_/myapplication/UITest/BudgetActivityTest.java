11fed3d18500449cab8db1bf8c5f194a6bd7711e
MaysaraHolmes <maysaradeveloper@gmail.com>
2018-04-15 00:00:30 +0300

    create new Budget test

--- app/src/androidTest/java/com/example/maysara_/myapplication/UITest/BudgetActivityTest.java
+++ app/src/androidTest/java/com/example/maysara_/myapplication/UITest/BudgetActivityTest.java
@@ -7,12 +7,14 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import com.example.maysara_.myapplication.Activities.BudgetActivity;
 import com.example.maysara_.myapplication.Activities.ProfileActivity;
 import com.example.maysara_.myapplication.Helpers.DB_Helper;
 import com.example.maysara_.myapplication.Helpers.SharedPref;
 import com.example.maysara_.myapplication.R;
+import com.example.maysara_.myapplication.RecyclerViewMatcher;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,18 +24,28 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.example.maysara_.myapplication.TestUtils.withRecyclerView;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+//import static com.example.maysara_.myapplication.TestUtils.*;
+import static com.example.maysara_.myapplication.RecyclerViewMatcher.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class BudgetActivityTest {
+
+    private static final String  budgetNameString = "Budget";
+    private static final double  budgetBalance = 1000d;
 
     @Rule
     public ActivityTestRule<BudgetActivity> mActivityRule =
@@ -51,11 +63,29 @@ public class BudgetActivityTest {
     }
     @Test
     public void checkIfAddBudgetDisplaysDialog(){
-
-        onView(withId(R.id.addBudget)).perform(click());
+        clickOnFab();
         onView(withId(R.id.newBudgetName)).check(matches(isDisplayed()));
-
     }
+
+    @Test
+    public void createNewBudget(){
+        clickOnFab();
+        addBudget();
+        onView(withRecyclerView(R.id.budget_list).atPositionOnView(0, R.id.budgetName)).check(matches(withText(budgetNameString)));
+        onView(withRecyclerView(R.id.budget_list).atPositionOnView(0, R.id.budgetBalance)).check(matches(withText(String.valueOf(budgetBalance)+"$")));
+    }
+
+    private void clickOnFab(){
+        onView(withId(R.id.addBudget)).perform(click());
+    }
+
+    private void addBudget(){
+        onView(withId(R.id.newBudgetName)).perform(typeText(budgetNameString));
+        onView(withId(R.id.newBudgetBalance)).perform(clearText());
+        onView(withId(R.id.newBudgetBalance)).perform(typeText("1000"));
+        onView(withId(R.id.addNewBudget)).perform(click());
+    }
+
     @After
     public void tearDown(){
         db_helper.clearDB();
