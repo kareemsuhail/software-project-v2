cf3e44cb2ee7203b7f08eda568edd8eefee4d7d4
kareem ayesh <1kayesh@students.iugaza.edu.ps>
2018-04-14 22:06:35 +0300

    =adding Budget dialog test

--- app/src/androidTest/java/com/example/maysara_/myapplication/UITest/BudgetActivityTest.java
+++ app/src/androidTest/java/com/example/maysara_/myapplication/UITest/BudgetActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.maysara_.myapplication.UITest;
 
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.test.InstrumentationRegistry;
@@ -7,52 +8,61 @@ import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.maysara_.myapplication.Activities.BudgetActivity;
 import com.example.maysara_.myapplication.Activities.ProfileActivity;
+import com.example.maysara_.myapplication.Helpers.DB_Helper;
 import com.example.maysara_.myapplication.Helpers.SharedPref;
 import com.example.maysara_.myapplication.R;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class LoginPageTest {
+public class BudgetActivityTest {
 
     @Rule
-    public ActivityTestRule<ProfileActivity> mActivityRule =
-            new ActivityTestRule(ProfileActivity.class);
+    public ActivityTestRule<BudgetActivity> mActivityRule =
+            new ActivityTestRule(BudgetActivity.class);
+    private DB_Helper db_helper;
     @Before
     public void setUp(){
+        Context context = InstrumentationRegistry.getTargetContext();
+        db_helper = new DB_Helper(context);
+        db_helper.clearDB();
         SharedPreferences preferences = mActivityRule.getActivity().getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.clear();
         editor.commit();
     }
     @Test
-    public void phoneNumberShouldAcceptJustNumbersTest(){
-        onView(withId(R.id.phone)).perform(typeText(String.valueOf("15")));
-        onView(withText("15")).check(matches(isDisplayed()));
-        onView(withId(R.id.phone)).perform(replaceText(String.valueOf("")));
-        onView(withId(R.id.phone)).perform(typeText(String.valueOf("text")));
-        onView(withId(R.id.phone)).check(matches(withText("")));
-    }
-    @Test
-    public void usernameShouldNotContainNumbersTest(){
-        onView(withId(R.id.name)).perform(typeText("kareem ayesh"));
-        onView(withId(R.id.name)).check(matches(withText("kareem ayesh")));
-        onView(withId(R.id.name)).perform(replaceText(""));
-        onView(withId(R.id.name)).perform(typeText("kareem123 ayesh"));
-        onView(withId(R.id.name)).check(matches(withText("kareem ayesh")));
+    public void checkIfAddBudgetDisplaysDialog(){
+
+        onView(withId(R.id.addBudget)).perform(click());
+        onView(withId(R.id.newBudgetName)).check(matches(isDisplayed()));
 
     }
+    @After
+    public void tearDown(){
+        db_helper.clearDB();
+        SharedPreferences preferences = mActivityRule.getActivity().getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.commit();
+    }
+
 }
